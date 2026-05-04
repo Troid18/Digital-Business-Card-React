596832290288d6f983d11632052e25b9e7178152
Musa Maphutha <josephmaphutha17@gmail.com>
2026-05-04 20:52:15 +0200

    feat: fixed the media designs for types of screens

--- react/src/components/Footer.jsx
+++ react/src/components/Footer.jsx
@@ -1,11 +1,11 @@
 function Footer(){
     return <footer>
         <div className="foot">
-            <a href="#" className="git ">
+            <a href="https://github.com/Troid18" target="_blank" className="git ">
             <img className="icon" src="data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 64 64' aria-labelledby='title' aria-describedby='desc' role='img' xmlns:xlink='http://www.w3.org/1999/xlink'%3E%3Ctitle%3EGithub%3C/title%3E%3Cdesc%3EA line styled icon from Orion Icon Library.%3C/desc%3E%3Cpath data-name='layer1' d='M32 1.952a30.019 30.019 0 0 0-9.469 58.5c1.5.281 2.063-.656 2.063-1.406v-5.625c-8.344 1.779-10.125-3.563-10.125-3.563-1.406-3.469-3.375-4.406-3.375-4.406-2.719-1.875.187-1.781.187-1.781 3 .188 4.594 3.094 4.594 3.094 2.719 4.594 7.031 3.281 8.719 2.531a6.5 6.5 0 0 1 1.875-4.031c-6.656-.75-13.688-3.375-13.688-14.812a11.5 11.5 0 0 1 3.094-8.063 11.217 11.217 0 0 1 .281-7.969s2.531-.844 8.25 3.094a28.944 28.944 0 0 1 7.5-1.031 28.4 28.4 0 0 1 7.5 1.031c5.719-3.844 8.25-3.094 8.25-3.094a11.217 11.217 0 0 1 .281 7.969 11.34 11.34 0 0 1 3.094 8.063c0 11.531-7.031 14.063-13.688 14.813a7.262 7.262 0 0 1 2.063 5.534v8.25c0 .844.562 1.687 2.063 1.406A30.019 30.019 0 0 0 32 1.952z' fill='none' stroke='%23202020' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' stroke-linejoin='round'%3E%3C/path%3E%3C/svg%3E" alt="Github" />
         </a>
 
-        <a href="#">
+        <a href="https://www.linkedin.com/in/joseph-maphutha-699417381/" target="_blank">
             <img className="icon" src="data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 64 64' aria-labelledby='title' aria-describedby='desc' role='img' xmlns:xlink='http://www.w3.org/1999/xlink'%3E%3Ctitle%3ELinkedin%3C/title%3E%3Cdesc%3EA line styled icon from Orion Icon Library.%3C/desc%3E%3Cpath data-name='layer1' 
                         fill='none' stroke='%23202020' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M3.078 22.331h12.188v36.844H3.078z' stroke-linejoin='round'%3E%3C/path%3E%3Cpath data-name='layer2' d='M46.719 21.112c-5.344 0-8.531 1.969-11.906 6.281v-5.062H22.625v36.844h12.281V39.206c0-4.219 2.156-8.344 7.031-8.344s7.781 4.125 7.781 
                         8.25v20.063H62V38.269c0-14.532-9.844-17.157-15.281-17.157z' fill='none' stroke='%23202020' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' stroke-linejoin='round'%3E%3C/path%3E%3Cpath data-name='layer1' d='M9.219 4.425C5.188 4.425 2 7.331 2 10.894s3.188 6.469 7.219 6.469 7.219-2.906 7.219-6.469-3.188-6.469-7.219-6.469z' 
--- react/src/components/Header.jsx
+++ react/src/components/Header.jsx
@@ -13,7 +13,7 @@ function Header(){
 
                 <div className="links ">
 
-                    <a href="#" className="link email">
+                    <a href="mailto:mjmaphutha03@gmail.com" className="link email">
                     <img src="data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 64 64' aria-labelledby='title' aria-describedby='desc' 
                     role='img' xmlns:xlink='http://www.w3.org/1999/xlink'%3E%3Ctitle%3EMail%3C/title%3E%3Cdesc%3EA solid styled icon from Orion Icon Library.%3C/desc%3E%3Cpath 
                     data-name='layer1' fill='%23202020' d='M2 14.7v34.4l17.2-17.5L2 14.7zm42.8 16.9L62 49.1V14.7L44.8 31.6z'%3E%3C/path%3E%3Cpath data-name='layer2' fill='%23202020' 
@@ -23,7 +23,7 @@ function Header(){
 
                     </a>
 
-                    <a href="#" className="link linkedin">
+                    <a href="https://www.linkedin.com/in/joseph-maphutha-699417381/" className="link linkedin" target="_blank">
                         <img src="data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 64 64' aria-labelledby='title' aria-describedby='desc' role='img' xmlns:xlink='http://www.w3.org/1999/xlink'%3E%3Ctitle%3ELinkedin%3C/title%3E%3Cdesc%3EA line styled icon from Orion Icon Library.%3C/desc%3E%3Cpath data-name='layer1' 
                         fill='none' stroke='%23202020' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M3.078 22.331h12.188v36.844H3.078z' stroke-linejoin='round'%3E%3C/path%3E%3Cpath data-name='layer2' d='M46.719 21.112c-5.344 0-8.531 1.969-11.906 6.281v-5.062H22.625v36.844h12.281V39.206c0-4.219 2.156-8.344 7.031-8.344s7.781 4.125 7.781 
                         8.25v20.063H62V38.269c0-14.532-9.844-17.157-15.281-17.157z' fill='none' stroke='%23202020' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' stroke-linejoin='round'%3E%3C/path%3E%3Cpath data-name='layer1' d='M9.219 4.425C5.188 4.425 2 7.331 2 10.894s3.188 6.469 7.219 6.469 7.219-2.906 7.219-6.469-3.188-6.469-7.219-6.469z' 
